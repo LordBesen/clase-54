af5e85911b9f84b83b6065cf1369ee431dfada97
Federico Besenzoni <besen@hotmail.com>
2021-06-08 19:42:03 -0300

    CLima app 2

--- models/busquedas.js
+++ models/busquedas.js
@@ -14,7 +14,7 @@ class Busquedas {
 
         try{
         
-        const resp = await axios.get('https://reqres.in/api/users?page=2');
+        const resp = await axios.get('https://api.mapbox.com/geocoding/v5/mapbox.places/ottawa.json?access_token=pk.eyJ1IjoiYmVzZW4iLCJhIjoiY2twb202OTN0Mm0wZTJwbXd4N3JrMTl2aCJ9.AZ07E8HaGf0oh2FExaPIqA&limit=5&language=es');
         console.log(resp.data);
         return []; // regresar resultados
     } catch (error) {
