89aa2fab5798f4dbddae9eb730d9beda1a6cdf31
Federico Besenzoni <besen@hotmail.com>
2021-06-08 20:14:23 -0300

    clima app 3

--- models/busquedas.js
+++ models/busquedas.js
@@ -10,11 +10,28 @@ class Busquedas {
         //leer db si existe
     }
 
+
+    get paramsMapbox() {
+        return {
+            'access_token': 'pk.eyJ1IjoiYmVzZW4iLCJhIjoiY2twb202OTN0Mm0wZTJwbXd4N3JrMTl2aCJ9.AZ07E8HaGf0oh2FExaPIqA',
+            'limit': 5,
+            'Language': 'es'
+        }
+
+    }
+
     async ciudad(lugar = '') {
 
         try{
+
+            const instance = axios.create({
+                baseURL:`https://api.mapbox.com/geocoding/v5/mapbox.places/${lugar}.json `,
+                params: this.paramsMapbox,
+
+            });
+            const resp = await instance.get();
         
-        const resp = await axios.get('https://api.mapbox.com/geocoding/v5/mapbox.places/ottawa.json?access_token=pk.eyJ1IjoiYmVzZW4iLCJhIjoiY2twb202OTN0Mm0wZTJwbXd4N3JrMTl2aCJ9.AZ07E8HaGf0oh2FExaPIqA&limit=5&language=es');
+       
         console.log(resp.data);
         return []; // regresar resultados
     } catch (error) {
@@ -25,6 +42,7 @@ class Busquedas {
 
 
 }
+
 
 }
 
